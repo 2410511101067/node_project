d097cb49c298f4a318458add668b628f64e3e19c
Sirinphat Darawankul <2410511101067utcc@gmail.com>
2025-04-06 19:35:19 +0700

    **edit** - add 2 more test cases

--- myrestapi.test.js
+++ myrestapi.test.js
@@ -5,9 +5,9 @@ describe('test hr zone calculation', () => {
     expect(restapi.calculateHR(30,180).zone).toBe(5);
   });
   test('test - lb hr is correct?', () => {
-    expect(restapi.calculateHR(30,180).zone).toBe(171);
+    expect(restapi.calculateHR(30,180).lb).toBe(171);
   });
   test('test - ub hr is correct?', () => {
-    expect(restapi.calculateHR(30,180).zone).toBe(190);
+    expect(restapi.calculateHR(30,180).ub).toBe(190);
   });
 });
